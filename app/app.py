ebe77aa711f8cf8af9c6548670eab7d5d8b8151b
William Zhang <william.zhang1093@gmail.com>
2026-02-19 18:00:07 -0500

    build: dependencies

--- app/app.py
+++ app/app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from pymongo import MongoClient
+from dotenv import load_dotenv, dotenv_values
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -6,5 +11,19 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route("/mongo/ping/")
+def mongo_ping():
+    try:
+        client.admin.command("ping")
+        return "MongoDB is working!"
+    except Exception as e:
+        return f"MongoDB is not working: {e}"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    client = MongoClient(os.getenv("MONGO_URL"))
+    db = client.get_database(os.getenv("MONGO_DBNAME"))
+    app.run(
+        debug=True,
+        host=os.getenv("FLASK_HOST"),
+        port=os.getenv("FLASK_PORT", 5000)
+    )
