c6e8dfa51e66545b5f2e45bd129c06a2820b8bf8
William Zhang <william.zhang1093@gmail.com>
2026-02-21 18:08:29 -0500

    feat(api): dining hall and food

--- app/app.py
+++ app/app.py
@@ -1,47 +1,56 @@
 from flask import Flask
 from pymongo import MongoClient
 from dotenv import load_dotenv, dotenv_values
-from flask import jsonify, request
+from flask import jsonify, request, Response
+from bson.json_util import dumps
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+def respond(status_code=200, data=None):
+    if status_code == 200:
+        return Response(
+            dumps({"ok": True, "data": data}),
+            mimetype="application/json",
+        ), status_code
+    else:
+        return Response(
+            dumps({"ok": False, "message": data}),
+            mimetype="application/json",
+        ), status_code
 
 @app.route("/halls/list", methods=["GET"]) # US01
 def get_halls():
     try:
         halls = db.halls.find({}, {"_id": 0, "id": 1, "name": 1})
-        return jsonify({"ok": True, "data": halls}, status=200)
+        return respond(data=list(halls))
     except Exception as e:
-        print("Encountered error in /halls/list: ",e.with_traceback())
-        return jsonify({"ok": False}, status=500)
+        print(f"Encountered error in /halls/list: {e}")
+        return respond(500)
 
 @app.route("/halls/<hall_id>", methods=["GET"]) # US02
 def get_hall_details(hall_id):
     try:
         hall = db.halls.find_one({"id": hall_id})
         if hall is None:
-            return jsonify({"ok": False, "message": "Unknown hall ID"}, status=404)
-        return jsonify({"ok": True, "data": hall}, status=200)
+            return respond(404, "Unknown hall ID")
+        return respond(data=hall)
     except Exception as e:
-        print(f"Encountered error in /halls/{hall_id}: ",e.with_traceback())
-        return jsonify({"ok": False}, status=500)
+        print(f"Encountered error in /halls/{hall_id}: {e}")
+        return respond(500)
 
 @app.route("/foods/<food_item_id>", methods=["GET"]) # US03
 def get_food_item_details(food_item_id):
     try:
         food_item = db.foods.find_one({"id": food_item_id})
         if food_item is None:
-            return jsonify({"ok": False, "message": "Unknown food item ID"}, status=404)
-        return jsonify({"ok": True, "data": food_item}, status=200)
+            return respond(404, "Unknown food item ID")
+        return respond(data=food_item)
     except Exception as e:
-        print(f"Encountered error in /foods/{food_item_id}: ",e.with_traceback())
-        return jsonify({"ok": False}, status=500)
+        print(f"Encountered error in /foods/{food_item_id}: {e}")
+        return respond(500)
 
 @app.route("/foods/search", methods=["POST"]) # US04, US05, US06
 def search_food_items():
@@ -53,17 +62,34 @@ def search_food_items():
         "tags": [tag_id, tag_id, ...]
     }
     '''
-    data = request.json
-    #return jsonify({"message": f"Searching for food items by name {data.get("name", None)}, hall ID {data.get("hall_id", None)}, tags {data.get("tags", None)}"})
-    return jsonify({"message": "Not Implemented"}, status=501)
+    try:
+        data = request.json
+        foods = None
+        print(f"Data: {data}")
+        if data.get("name", None) is not None:
+            foods = db.foods.find({"name": {"$regex": data.get("name", None), "$options": "i"}})
+        elif data.get("hall_id", None) is not None:
+            foods = db.foods.find({"foodEdges.0": data.get("hall_id", None)})
+        elif data.get("tags", None) is not None:
+            foods = db.foods.find({"filters.id": {"$in": data.get("tags", None)}})
+        else:
+            return respond(400, "No search criteria provided")
+        if foods is None:
+            return respond(404, "No foods found")
+        return respond(data=foods)
+    except Exception as e:
+        print(f"Encountered error in /foods/search: {e}")
+        print(f"Data: {request.json}")
+        return respond(500)
 
 @app.route("/config/tags", methods=["GET"]) # Dependencies of US06
 def get_tags():
-    return jsonify({"message": "Not Implemented"}, status=501)
-
-@app.route("/refresh", methods=["POST"])
-def refresh():
-    return jsonify({"message": "Not Implemented"}, status=501)
+    try:
+        tags = db.tags.find({})
+        return respond(data=tags)
+    except Exception as e:
+        print(f"Encountered error in /config/tags: {e}")
+        return respond(500)
 
 @app.route("/mongo/ping/")
 def mongo_ping():
