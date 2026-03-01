fa0c6de29ca98c919a5e2d7820c7921b2bbab703
hewlett-packard-lovecraft <hx2314@nyu.edu>
2026-02-28 20:11:38 -0500

    update login and registration styling

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, render_template, session
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -11,11 +10,10 @@ from auth import auth_bp
 from foods import foods_bp
 from api import api_bp
 
-import datetime
 
 app = Flask(__name__)
 
-app.secret_key = os.getenv("SECRET_KEY", "dev-only-change-me")
+app.secret_key = config.SECRET_KEY
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(foods_bp)
@@ -23,33 +21,44 @@ app.register_blueprint(api_bp)
 
 #### Pages ####
 
+
 @app.route("/home")
 def home():
     halls = list(db.halls.find())
     return render_template("home.html", title="Home", halls=halls, show_header=False)
 
+
 @app.route("/halls")
 def halls_list():
     halls = list(db.halls.find())
-    return render_template("halls.html", title="Dining Halls", halls=halls, show_header=False)
+    return render_template(
+        "halls.html", title="Dining Halls", halls=halls, show_header=False
+    )
+
 
 @app.route("/halls")
 def halls():
     halls = list(db.halls.find())
     return render_template("halls.html", title="Halls", halls=halls, show_header=False)
 
+
 #### Authenticated ####
+
 
 @app.route("/account")
 @require_auth
 def account_page(user_id):
-    user = db.users.find_one({"_id": ObjectId(user_id)}, {"_id": 0, "username": 1, "email": 1, "netid": 1})
+    user = db.users.find_one(
+        {"_id": ObjectId(user_id)}, {"_id": 0, "username": 1, "email": 1, "netid": 1}
+    )
     if user is None:
         return respond(404, "User not found")
     return "NOT_IMPLEMENTED"
-    #return render_template("account.html", title="Account", user=user, show_header=False)
+    # return render_template("account.html", title="Account", user=user, show_header=False)
+
 
 #### Status ####
+
 
 @app.route("/mongo/ping/")
 def mongo_ping():
@@ -60,9 +69,9 @@ def mongo_ping():
         return f"MongoDB is not working: {e}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
         debug=True,
         host=config.FLASK_HOST or "0.0.0.0",
-        port=int(config.FLASK_PORT or 5000)
+        port=int(config.FLASK_PORT or 5000),
     )
